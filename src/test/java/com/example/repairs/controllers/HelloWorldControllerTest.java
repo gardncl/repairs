899dbb671411ecae09f0cbab8993e7ec436c4d1b
cwwagne2 <cwwagne2@ncsu.edu>
2020-12-29 13:43:04 -0500

    tests for passing name parameter

--- src/test/java/com/example/repairs/controllers/HelloWorldControllerTest.java
+++ src/test/java/com/example/repairs/controllers/HelloWorldControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -16,8 +17,24 @@ class HelloWorldControllerTest {
     private MockMvc mockMvc;
 
 
+    /**
+     * Tests the return content of the url '/hello' when no name parameter is passed
+     * @throws Exception
+     */
     @Test
     void getGreetingWithoutName() throws Exception {
-        mockMvc.perform(get("/hello")).andExpect(status().isOk()).andReturn();
+        mockMvc.perform(get("/hello")).andExpect(status().isOk());
+        mockMvc.perform(get("/hello")).andExpect(content().string("Hello World")).andReturn();
+    }
+
+    /**
+     * Tests the return content of the url '/hello?name=[name]' where [name] is replaced by
+     * any string value.
+     * @throws Exception
+     */
+    @Test
+    void getGreetingWithName() throws Exception {
+        mockMvc.perform(get("/hello?name=Chris")).andExpect(status().isOk());
+        mockMvc.perform((get("/hello?name=Chris"))).andExpect(content().string("Hello Chris")).andReturn();
     }
 }
